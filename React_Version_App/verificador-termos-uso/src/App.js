2d680fb90b05dba297482b0b472329f530c6beba
Paula Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-08-28 13:34:07 -0300

    Add Markdown Parsing and Rendering
    Added the functionality to parse and render Markdown content using the 'marked' library. The App component now utilizes the 'marked' library to parse the input text and display the formatted content in the summary section. The 'verificarTermo' function processes the Markdown content and updates the summary state accordingly.

--- React_Version_App/verificador-termos-uso/src/App.js
+++ React_Version_App/verificador-termos-uso/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as marked from 'marked';
+import TermoUsoInput from './components/TermosDeUsoInput';
+import Resumo from './components/Resumo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [termo, setTermo] = useState('');
+    const [resumo, setResumo] = useState('');
+
+    const verificarTermo = () => {
+        const parsedTermo = marked(termo);
+
+        setResumo(parsedTermo);
+    };
+
+    const limparTexto = () => {
+        setTermo('');
+    };
+
+    return (
+        <div className="App">
+            <h1>Verificador de Termos de Uso</h1>
+            <TermoUsoInput onChange={(e) => setTermo(e.target.value)} value={termo} />
+            <button onClick={verificarTermo}>Verificar</button>
+            <Resumo resumo={resumo} />
+        </div>
+    );
 }
 
 export default App;
