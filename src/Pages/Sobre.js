240d74af15a6eb3540fde5bb7a88593d57648826
Paula Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-11-16 22:44:02 -0300

    Adiciona melhorias na página 'Sobre':
    - Refina o texto explicativo, detalhando o propósito do [Nome do Sistema].
    - Descreve como a aplicação funciona, destacando sua utilidade na simplificação de termos de uso.
    - Atualiza agradecimentos e compromisso com o usuário.
    - Introduz uma seção de compromisso e destaca o desenvolvimento do sistema como parte de um Trabalho de Conclusão de Curso (TCC).
    - Personaliza o nome do sistema para 'SysATU'.
    - Adiciona responsividade aos estilos do cabeçalho para melhor experiência em telas pequenas.

--- src/Pages/Sobre.js
+++ src/Pages/Sobre.js
@@ -4,17 +4,24 @@ import './Sobre.css';
 function Sobre() {
   return (
     <>
-      <h2 className='tituloSobre'>Bem-vindo à Página Sobre</h2>
+      <h2 className='tituloSobre'>Bem-vindo ao SysATU!</h2>
 
       <div className='textoSobre'>
         <p>
-          Nós somos dedicados a fornecer informações úteis e relevantes
-          para nossos usuários, com o objetivo de facilitar a compreensão dos termos de uso. Se você tiver
-          alguma dúvida ou sugestão, não hesite em entrar em contato conosco.
+          Desenvolvido como parte de um Trabalho de Conclusão de Curso (TCC), nosso compromisso é oferecer informações valiosas e pertinentes para os usuários. Nosso foco principal é simplificar a compreensão dos intricados termos de uso que frequentemente encontramos na web.
+        </p>
+        <h3>Como Funciona:</h3>
+        <p>
+          O SysATU é uma ferramenta intuitiva projetada para tornar a leitura e entendimento dos termos de uso uma tarefa fácil. Você pode simplesmente inserir o texto dos termos no espaço fornecido, e nossa aplicação destacará e explicará os pontos mais importantes. Isso permite que você navegue pelos termos de maneira mais eficiente, economizando tempo e esforço.
+        </p>
+
+        <h3>Nosso Compromisso:</h3>
+        <p>
+          Estamos dedicados a proporcionar a melhor experiência possível. Seja para esclarecer dúvidas ou receber sugestões de aprimoramento, valorizamos o contato direto com nossos usuários. Sua satisfação é a nossa prioridade.
         </p>
         <p>
-          Nosso objetivo é tornar sua experiência a melhor possível. Agradecemos por escolher nosso serviço
-          e esperamos que você encontre tudo o que precisa aqui.
+          Agradecemos por escolher o SysATU. Esperamos que encontre tudo o que precisa para uma compreensão mais clara e descomplicada dos termos de uso.
+          Para qualquer dúvida ou sugestão, não hesite em entrar em contato conosco. Estamos aqui para ajudar!
         </p>
       </div>
     </>
